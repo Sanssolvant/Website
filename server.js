461f4b43968a2a7f5b9e635b428488ad283d231b
Zinedine Tomasella <zinedine.tomasella@hispeed.ch>
2022-10-23 22:05:51 +0200

    PORT 5000 to 10000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const oAuth2Client = new google.auth.OAuth2(
 	process.env.CLIENTID,
