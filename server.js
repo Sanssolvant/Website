d52d3aa698e2ec8865f98d0ea56d17e3a7ad78eb
Zinedine Tomasella <zinedine.tomasella@hispeed.ch>
2022-10-23 21:31:06 +0200

    Changes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
-const { google, GoogleApis } = require("googleapis");
+const { google } = require("googleapis");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 const oAuth2Client = new google.auth.OAuth2(
 	process.env.CLIENTID,
 	process.env.CLIENTKEY,
 	process.env.REDIRECTURI
 );
-oAuth2Client.setCredentials({ refreshToken: process.env.REFRESHTOKEN });
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESHTOKEN });
 
 // Middleware
 app.use(express.static("public"));
@@ -23,49 +23,50 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
 	console.log(req.body);
+	const accessToken = oAuth2Client.getAccessToken();
+
+	const transporter = nodemailer.createTransport({
+		host: "smtp.gmail.com",
+		port: 465,
+		secure: true,
+		auth: {
+			type: "OAuth2",
+			user: process.env.EMAIL,
+			pass: process.env.PASSWORD,
+			clientId: process.env.CLIENTID,
+			clientSecret: process.env.CLIENTKEY,
+			refreshToken: process.env.REFRESHTOKEN,
+			accessToken: accessToken,
+		},
+	});
+
+	let mailOptions = {
+		from: req.body.email,
+		to: process.env.EMAIL,
+		subject: `Message from ${req.body.email}: ${req.body.subject}`,
+		text: req.body.message,
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.log(error);
+			res.send("error");
+		} else {
+			console.log("Email sent: " + info.response);
+			res.send("Success");
+		}
+	});
+
+	transporter.verify(function (error, success) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("Server is ready to take our messages");
+		}
+		transporter.close();
+	});
 });
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
-	sendMail();
 });
-
-async function sendMail() {
-	try {
-		const accessToken = await oAuth2Client.getAccessToken();
-
-		const transporter = nodemailer.createTransport({
-			host: "smtp.gmail.com",
-			port: 465,
-			secure: true,
-			auth: {
-				type: "OAuth2",
-				user: process.env.EMAIL,
-				pass: process.env.PASSWORD,
-				clientId: process.env.CLIENTID,
-				clientSecret: process.env.CLIENTKEY,
-				refreshToken: process.env.REFRESHTOKEN,
-				accessToken: accessToken,
-			},
-		});
-
-		let mailOptions = {
-			from: req.body.email,
-			to: process.env.EMAIL,
-			subject: `Message from ${req.body.email}: ${req.body.subject}`,
-			text: req.body.message,
-		};
-
-		const result = await transporter.sendMail(mailOptions, (error, info) => {
-			if (error) {
-				console.log(error);
-				res.send("error");
-			} else {
-				console.log("Email sent: " + info.response);
-				res.send("Success");
-			}
-		});
-	} catch (error) {
-		return error;
-	}
-}
